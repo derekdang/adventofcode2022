7467dba7156d7dde9be149509d67a8e406a1524b
Derek Dang <21960193+derekdang@users.noreply.github.com>
2022-12-12 10:01:16 -0800

    Day 12 Solutions

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2022/day/12
 import sys
+import re
 
 def traverse(grid, steps_to_point, step_no, prev_elev, x, y, w,h):
     if (x < 0 or x >= w or y < 0 or y >= h or step_no > steps_to_point[y][x]):
@@ -17,56 +18,90 @@ def traverse(grid, steps_to_point, step_no, prev_elev, x, y, w,h):
 def print_2d_array(arr):
     for l in arr:
         print(l)
+    print('\n')
 
-def is_valid_move(grid,new_elev,x,y,w,h):
+def is_valid_move(grid,prev_elevation,x,y,w,h):
     if (x < 0 or x >= w or y < 0 or y >= h):
         return False
-    if grid[y][x] != 'S':
-        if new_elev > ord(grid[y][x])+1:
-            return False
+    if (prev_elevation+1 < ord(grid[y][x])): #  or (chr(prev_elevation) != 'z' and grid[y][x] == 'E')
+        return False
     return True
-
-def calc_min_step(grid,steps_to_point,x,y,w,h):
-    if (x < 0 or x >= w or y < 0 or y >= h):
-        return 
-    elev = ord(grid[y][x])
     
-    up,down,left,right = 10000,10000,10000,10000
-    if is_valid_move(grid,elev,x,y-1,w,h):
-        up = steps_to_point[y-1][x]
-    if is_valid_move(grid,elev,x,y+1,w,h):
-        down = steps_to_point[y+1][x]
-    if is_valid_move(grid,elev,x-1,y,w,h):
-        left = steps_to_point[y][x-1]
-    if is_valid_move(grid,elev,x+1,y,w,h):
-        right = steps_to_point[y][x+1]
-    
-    min_move = min(up,down,left,right)
-    if min_move != 10000:
-        steps_to_point[y][x] = min(up,down,left,right)+1
+def add_valid_moves(grid,elevation,x,y,w,h,locations_to_visit,step,visited):
+    if is_valid_move(grid,elevation,x,y-1,w,h) and not visited[y-1][x]:
+        if [x,y-1,step] not in locations_to_visit:
+            locations_to_visit.append([x,y-1,step])
+    if is_valid_move(grid,elevation,x,y+1,w,h) and not visited[y+1][x]:
+        if [x,y+1,step] not in locations_to_visit:
+            locations_to_visit.append([x,y+1,step])
+    if is_valid_move(grid,elevation,x-1,y,w,h) and not visited[y][x-1]:
+        if [x-1,y,step] not in locations_to_visit:
+            locations_to_visit.append([x-1,y,step])
+    if is_valid_move(grid,elevation,x+1,y,w,h) and not visited[y][x+1]:
+        if [x+1,y,step] not in locations_to_visit:
+            locations_to_visit.append([x+1,y,step])
 
 if __name__ == "__main__":
     grid = open(sys.argv[1]).read().splitlines()
     w,h = len(grid[0]), len(grid)
     steps_to_point = [[10000 for x in range(w)] for y in range(h)]
+    visited = [[False for x in range(w)] for y in range(h)]
     start_y,start_x = -1,-1
     end_y,end_x = -1,-1
+    locations_to_visit = []
+    all_possible_starting = [] # part 2
     for i,line in enumerate(grid):
         if 'S' in line:
             start_y = i
             start_x = line.find('S')
             steps_to_point[start_y][start_x] = 0
-            elev = ord('a')
-            # traverse(grid,steps_to_point,1,elev,start_x-1,start_y,w,h)
-            # traverse(grid,steps_to_point,1,elev,start_x+1,start_y,w,h)
-            # traverse(grid,steps_to_point,1,elev,start_x,start_y-1,w,h)
-            # traverse(grid,steps_to_point,1,elev,start_x,start_y+1,w,h)
+            visited[start_y][start_x] = True
+            # add the valid moves
+            add_valid_moves(grid,ord('a'),start_x,start_y,w,h,locations_to_visit,1,visited)
         if 'E' in line:
             end_y = i
             end_x = line.find('E')
+        
+        #part 2
+        all_index_of_a = [m.start() for m in re.finditer('a',line)]
+        for index in all_index_of_a:
+            all_possible_starting.append([index,i])
+    
+    #bfs while loc to visits is not empty
+    while(len(locations_to_visit) != 0):
+        point = locations_to_visit.pop(0)
+        steps_to_point[point[1]][point[0]] = point[2]
+        visited[point[1]][point[0]] = True
 
-    # while(steps_to_point[end_y][end_x] == 10000):
-    #     for y,line in enumerate(grid):
-    #         for x,r in enumerate(line):
-    #             calc_min_step(grid,steps_to_point,x,y,w,h) 
-    print_2d_array(steps_to_point)
+        if point[0] == end_x and point[1] == end_y:
+            break
+        char = grid[point[1]][point[0]]
+        elevation = ord(grid[point[1]][point[0]])
+        add_valid_moves(grid, elevation,point[0],point[1],w,h,locations_to_visit,point[2]+1,visited)
+
+    min_step_to_point = steps_to_point[end_y][end_x]
+
+    visited = [[False for x in range(w)] for y in range(h)]
+    steps_to_point = [[10000 for x in range(w)] for y in range(h)]
+    locations_to_visit = []
+    print(f"Part 1 starting from S: {min_step_to_point}")
+    for starting_point in all_possible_starting:
+        s_x,s_y = starting_point[0],starting_point[1]
+        steps_to_point[s_y][s_x] = 0
+        visited[s_y][s_x] = True
+        add_valid_moves(grid,ord('a'),s_x,s_y,w,h,locations_to_visit,1,visited)
+        while(len(locations_to_visit) != 0):
+            point = locations_to_visit.pop(0)
+            steps_to_point[point[1]][point[0]] = point[2]
+            visited[point[1]][point[0]] = True
+
+            if point[0] == end_x and point[1] == end_y:
+                min_step_to_point = min(steps_to_point[end_y][end_x], min_step_to_point)
+                break
+            char = grid[point[1]][point[0]]
+            elevation = ord(grid[point[1]][point[0]])
+            add_valid_moves(grid, elevation,point[0],point[1],w,h,locations_to_visit,point[2]+1,visited)
+        visited = [[False for x in range(w)] for y in range(h)]
+        steps_to_point = [[10000 for x in range(w)] for y in range(h)]
+        locations_to_visit = []
+    print(f"Part 2 starting from any 'a': {min_step_to_point}")
